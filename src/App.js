b2e23fd14270a2cf161c9babfed23428b6396f6a
Leonidas <ldbrooks297@gmail.com>
2021-04-15 19:24:27 -0700

    App.js: initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-// imports
+// import modules
 import React, {useEffect, useState} from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+//import css
+import './App.css';
+
+//import components
+import Welcome from './components/Welcome';
+import Footer from './components/Footer';
+import MainCard from './components/MainCard';
 
 
 function App() {
@@ -9,9 +16,16 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="mainApp">
       {/*declare what is to be rendered on client-side*/}
-
+      <div className="nameHeader">
+        よろしくお願いします
+      </div>
+      <hr className="nameHeaderLine"/>
+      <Welcome/>
+      <MainCard/>
+      {/* include component that houses entire business card component*/}
+      <Footer/>
     </div>
   );
 }
