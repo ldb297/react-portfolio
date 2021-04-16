31d671a1913d7b5d1b9f5451fb06f383b247cf1b
Leonidas <ldbrooks297@gmail.com>
2021-04-15 19:26:05 -0700

    Welcome.js: empty for now
    possibly make nav

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            {/* potentially put some nav links and rename this component */}
+        </div>
+    )
+}
+
+export default Welcome;
