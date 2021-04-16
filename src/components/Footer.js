f3a748017202d3455e6ea69f904d38f1d85e6412
Leonidas <ldbrooks297@gmail.com>
2021-04-15 19:25:30 -0700

    Footer.js: create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+//import modules
+import React from 'react';
+
+//import styling
+import './styling/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="mainFooter">
+            @2021 Built by 新垣リオダンテー
+        </footer>
+    )
+}
+
+export default Footer;
