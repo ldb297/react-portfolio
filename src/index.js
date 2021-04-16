c1767a6c309fb86d25067122f322fa02119c7ec9
Leonidas <ldbrooks297@gmail.com>
2021-04-15 19:24:44 -0700

    initialize index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+//import modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//import css
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
