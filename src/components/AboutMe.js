f557e102e57f505abf68e69fd9e44506225f9e6c
Leonidas <ldbrooks297@gmail.com>
2021-04-22 13:06:46 -0700

    AboutMe.js:
    init new component
    
    include bio image
    resume download
    text bio

---
+++ src/components/AboutMe.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './styling/AboutMe.css';
+
+import LanguageInfo from './LanguageInfo';
+
+import resume from './styling/resume.pdf';
+import bioImage from './styling/bioImage.png';
+
+const AboutMe = () => {
+
+    return (
+        <>
+            <div className="aboutMeQuoteText">`driven by clean engineering architectures`</div>
+            
+            <div className="aboutMeBioContainer">
+                <div className="aboutMeImageContainer">
+                    <img id="picOfMe" src={bioImage} alt="photograph of me"/>
+                </div>
+
+                <textarea className="aboutMeTextContainer" disabled>
+                    Dominant in ES6, JS frameworks, and Python, I strive to build intuitive solutions for the complex problems of tomorrow. My biggest weakness is not being able to accurately measure my own strengths. My biggest strength is being able to see the weaknesses in my teammates and approach them with thoughtful and constructive advice with a pressureless demeanor. In my free time I enjoy trail-running, reading science fiction non-fiction, studying theoretical scientific texts, exploring my culinary creativity, and working through my Super Ultimate 12 sudoku book. Thank you for taking the time to visit my portfolio, I hope you've enjoyed your visit.
+                </textarea>
+            </div>
+            
+            <a className="resumeLink" href={resume} download="Leo_Brooks_Design_Resume" target="_blank" rel="noreferrer">Resume</a>
+            <LanguageInfo/>
+            <div className="aboutMeQuoteText">`passionate about innovative programming design`</div>
+        </>
+    )
+}
+
+export default AboutMe;
