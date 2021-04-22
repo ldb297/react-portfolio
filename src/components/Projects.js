3f992c7e2bd08e1389b6c100746abf37dfeaa8c1
Leonidas <ldbrooks297@gmail.com>
2021-04-22 15:41:38 -0700

    Projects.js:
    create skeleton for project component
    
    import all images
    
    create modular container for each project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,61 @@
 import React from 'react';
 
+//styling
+import './styling/Projects.css'
+
+//image imports
+import p1code from './styling/images/p1code.png';
+import p1live from './styling/images/p1live.png';
+import p2code from './styling/images/p2code.png';
+import p2live from './styling/images/p2live.png';
+import p3back from './styling/images/p3back.png';
+import p3front from './styling/images/p3front.png';
+import p3live from './styling/images/p3live.png';
+import p4back from './styling/images/p4back.png';
+import p4front from './styling/images/p4front.png';
+import p4live from './styling/images/p4live.png';
+
 const Projects = () => {
 
     return (
         <>
         <div className="mainProjectContainer">
-            <div className="projectDisplayContainer" id="projectOne">
-                <a id="projectOneDeployedLink" href="https://ldb297.github.io/Project-1/">Deployed Link</a>
-            </div>
+            {/* flex column*/}
 
-            <div className="projectDisplayContainer" id="projectTwo">
-                <a id="projectTwoDeployedLink" href="https://gamebook297.herokuapp.com/">Deployed Link</a>
+            <div className="projectDisplayContainer" id="projectFour">
+                <a className="githubLink" href="https://github.com/ldb297/python_visualizer" target="_blank" rel="noreferrer">Github Link</a>
+                <span className="deployedLink">Deployment Under Construction</span>
             </div>
 
             <div className="projectDisplayContainer" id="projectThree">
-                <a id="projectThreeDeployedLink" href="https://nipponrunner.herokuapp.com/">Deployed Link</a>
+                <a className="githubLink" href="https://github.com/jessica-au/nipponrunnerFrontend"  target="_blank" rel="noreferrer">Github Frontend</a>
+                <a className="githubLink" href="https://github.com/jessica-au/nipponrunner" target="_blank" rel="noreferrer">Github Backend</a>
+                <a className="deployedLink" href="https://nipponrunner.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a>
             </div>
 
-            <div className="projectDisplayContainer" id="projectFour">
-                <span id="projectFourDeployedLink">Deployment Under Construction</span>
+            <div className="projectDisplayContainer" id="projectTwo">
+                <span className="projectTitle">gameBook</span>
+                <div className="projectImage">
+                    <img className="codeImage" id="p2code" src={p2code}/>
+                    <img className="liveImage" id="p2live" src={p2live}/>
+                </div>
+                <div className="links">
+                    <a className="githubLink" href="https://github.com/ldb297/gamebook" target="_blank" rel="noreferrer">Github Link</a>
+                    <a className="deployedLink" href="https://gamebook297.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a>
+                </div>
+            </div>
+
+            <div className="projectDisplayContainer" id="projectOne">
+                {/* flex */}
+                <span className="projectTitle">[2B || !2B]</span>
+                <div className="projectImage">
+                    <img className="codeImage" id="p1code" src={p1code}/>
+                    <img className="liveImage" id="p1live" src={p1live}/>
+                </div>
+                <div className="links">
+                    <a className="githubLink" href="https://github.com/ldb297/Project-1" target="_blank" rel="noreferrer">Github Link</a>
+                    <a className="deployedLink" href="https://ldb297.github.io/Project-1/" target="_blank" rel="noreferrer">Deployed Link</a>
+                </div>
             </div>
         </div>
         </>
