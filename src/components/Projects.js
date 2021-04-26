5aacc9520ee29b0c4146d24b6b56ed663d4566c9
Leonidas <ldbrooks297@gmail.com>
2021-04-26 15:45:22 -0700

    remove code images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,9 +25,12 @@ const Projects = () => {
             <div className="projectDisplayContainer" id="projectFour">
                 <span className="projectTitle">Nutrifit Tracker</span>
                 <div className="projectImage">
-                    <div className="twoImages">
+                    {/* <div className="twoImages">
                         <img className="codeImage" src={p4front} id="p4front" alt="fourth project front end code snippet"/>
                         <img className="codeImage" src={p4back} id="p4back" alt="fourth project back end code snippet"/>
+                    </div> */}
+                    <div className="projectDescription">
+                        Nutrition stat tracking application utilizing python scripts. Features Django built in CRUD operations with user authenticaiton and protected routes as well as parsed data pulled from USDA nutrition API. Current deployment under construction. 
                     </div>
                     <img className="liveImage" src={p4live} alt="fourth project live"/>
                 </div>
@@ -40,9 +43,12 @@ const Projects = () => {
             <div className="projectDisplayContainer" id="projectThree">
                 <span className="projectTitle">nipponRunner</span>
                 <div className="projectImage">
-                    <div className="dualImages">
+                    {/* <div className="dualImages">
                         <img className="codeImage" id="p3back" src={p3back} alt="third project back end code snippet"/>
                         <img className="codeImage" id="p3front" src={p3front} alt="third project front end code snippet"/>
+                    </div> */}
+                    <div className="projectDescription">
+                        MERN application built to help increase hiragana reading proficiency. Incorporated custom built API housing necessary hiragana characters. Constructed programmatically generated styling based on state variables connected to User info.
                     </div>
                     <img className="liveImage" src={p3live} alt="third project live"/>
                 </div>
@@ -56,7 +62,10 @@ const Projects = () => {
             <div className="projectDisplayContainer" id="projectTwo">
                 <span className="projectTitle">gameBook</span>
                 <div className="projectImage">
-                    <img className="codeImage" id="p2code" src={p2code} alt="second project code snippet"/>
+                    {/* <img className="codeImage" id="p2code" src={p2code} alt="second project code snippet"/> */}
+                    <div className="projectDescription">
+                        Video game API based user messaging boards built out in Node.js. Incorporated neatly parsed information from RAWG API, protected user routes for dynamic comment functions with looping database information retrieval and display.
+                    </div>
                     <img className="liveImage" id="p2live" src={p2live} alt="second project live"/>
                 </div>
                 <div className="links">
@@ -69,7 +78,10 @@ const Projects = () => {
                 {/* flex */}
                 <span className="projectTitle">[2B || !2B]</span>
                 <div className="projectImage">
-                    <img className="codeImage" id="p1code" src={p1code} alt="first project code snippet"/>
+                    {/* <img className="codeImage" id="p1code" src={p1code} alt="first project code snippet"/> */}
+                    <div className="projectDescription">
+                        HTML5, CSS, ES6 web based fighting simulator. Algorithmic fighting sequences based on DOM manipulation of user inputs. Let me know if you get to the final boss, he's pretty strong lol.
+                    </div>
                     <img className="liveImage" id="p1live" src={p1live} alt="first project live"/>
                 </div>
                 <div className="links">
