e1b498a1f9da5a01defd6c4f498699d73f7858ca
Leonidas <ldbrooks297@gmail.com>
2021-04-26 15:39:53 -0700

    add welcome text

--- src/App.js
+++ src/App.js
@@ -25,7 +25,12 @@ function App() {
       <Navigation/>
       <Route path="/about" component={AboutMe}/>
       <Route path="/projects" component={Projects}/>
-      <MainCard/>
+      <div className="welcomeText">
+        Welcome to my portfolio. The links above will take you to my bio page and 
+        projects page. On my bio page, you can click any of the icons for more 
+        information. On my projects page, there are links below each image that
+        will take you to their respective locations. All links open in a separate browser window.
+      </div>
       {/* include component that houses entire business card component*/}
       <Footer/>
     </div>
