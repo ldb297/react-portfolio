bb05dd736a71c47f052832d732d9cb90df361248
Leonidas <ldbrooks297@gmail.com>
2021-04-28 14:56:44 -0700

    reroute about to landing page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ import './styling/Navigation.css';
 const Navigation = () => {
     return (
         <div className="navigationContainer">
-            <div className="aboutLink"><NavLink className="navigationLink" to="/about">About</NavLink></div> 
+            <div className="aboutLink"><NavLink className="navigationLink" to="/">About</NavLink></div> 
             <div className="projectsLink"><NavLink className="navigationLink" to="/projects">Projects</NavLink></div>
             {/* potentially put some nav links and rename this component */}
         </div>
