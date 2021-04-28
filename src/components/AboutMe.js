de855d109427572613f6d8442912bf829a2eb75f
Leonidas <ldbrooks297@gmail.com>
2021-04-28 15:38:37 -0700

    change textarea to div
    hide scroll wheel
    
    add line breaks

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,10 +17,22 @@ const AboutMe = () => {
                 <div className="aboutMeImageContainer">
                     <img id="picOfMe" src={bioImage} alt="photograph of me"/>
                 </div>
-
-                <textarea className="aboutMeTextContainer" disabled>
-                    Dominant in ES6, JS frameworks, and Python, I strive to build intuitive solutions for the complex problems of tomorrow. My biggest weakness is not being able to accurately measure my own strengths. My biggest strength is being able to see the weaknesses in my teammates and approach them with thoughtful and constructive advice with a pressureless demeanor. In my free time I enjoy trail-running, reading science fiction non-fiction, studying theoretical scientific texts, exploring my culinary creativity, and working through my Super Ultimate 12 sudoku book. Thank you for taking the time to visit my portfolio, I hope you've enjoyed your visit.
-                </textarea>
+                <div className="aboutMeRightContainer">
+                    <div className="aboutMeTextContainer">
+                        Dominant in ES6, JS frameworks, and Python, I strive to build intuitive solutions for the complex problems of tomorrow. 
+                        <br/><br/>
+                        My biggest weakness is not being able to accurately measure my own strengths. 
+                        <br/><br/>
+                        My biggest strength is being able to see the weaknesses in my teammates and approach them with thoughtful and constructive advice with a pressureless demeanor. 
+                        <br/><br/>
+                        In my free time I enjoy trail-running, reading science fiction non-fiction, studying theoretical scientific texts, exploring my culinary creativity, and working through my Super Ultimate 12 sudoku book. 
+                        <br/><br/>
+                        Thank you for taking the time to visit my portfolio, I hope you've enjoyed your visit.
+                    </div>
+                    <div id="emailText">
+                        email: ldbrooks297@gmail.com
+                    </div>
+                </div>
             </div>
             
             <ContactInfo/>
