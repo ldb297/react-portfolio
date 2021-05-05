55c02e0c6f8974c1e522e7ebcd03c911ddf2c55c
Leonidas <ldbrooks297@gmail.com>
2021-05-05 15:03:25 -0700

    update bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,11 +21,11 @@ const AboutMe = () => {
                     <div className="aboutMeTextContainer">
                         Dominant in ES6, JS frameworks, and Python, I strive to build intuitive solutions for the complex problems of tomorrow. 
                         <br/><br/>
-                        My biggest weakness is not being able to accurately measure my own strengths. 
+                        My biggest weakness is not being able to accurately measure my own strengths. I struggle to accept praise, so I have a tendency to overcompensate for this inability by over working myself at times. I'm working on improving this by being more vulnerable to positive reinforcement.
                         <br/><br/>
-                        My biggest strength is being able to see the weaknesses in my teammates and approach them with thoughtful and constructive advice with a pressureless demeanor. 
+                        My biggest strength is being able to see the weaknesses in my teammates and approach them with thoughtful and constructive advice with a pressureless demeanor. I've often been praised by colleagues as emphatic and understanding of every situation, regardless of relatability. 
                         <br/><br/>
-                        In my free time I enjoy trail-running, reading science fiction non-fiction, studying theoretical scientific texts, exploring my culinary creativity, and working through my Super Ultimate 12 sudoku book. 
+                        In my free time I enjoy trail-running and long distance road running. I also like leisurely reading science fiction non-fiction books, news articles, and theoretical scientific texts. If I'm not running or reading, I can also be found exploring my culinary creativity or working through my Super Ultimate 12 sudoku book (it's currently kicking my butt lol but i'm determined to beat it! (*•̀ᴗ•́*)و ̑̑). 
                         <br/><br/>
                         Thank you for taking the time to visit my portfolio, I hope you've enjoyed your visit.
                     </div>
